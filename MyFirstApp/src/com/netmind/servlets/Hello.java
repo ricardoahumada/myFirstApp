2d0a766bf1a51d4641beb4da0fff54377015a4eb
ricardo <ricardo@enmotionvalue.com>
2017-05-29 17:23:32 +0200

    modif hello

--- MyFirstApp/src/com/netmind/servlets/Hello.java
+++ MyFirstApp/src/com/netmind/servlets/Hello.java
@@ -14,13 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 public class Hello extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Hello() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+   
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
